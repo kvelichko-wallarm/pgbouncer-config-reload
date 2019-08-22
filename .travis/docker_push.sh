3febc9993c8445d04f913439c10f8a7f1569ce70
kvelichko <kvelichko@wallarm.com>
2019-08-22 17:31:30 +0300

    Add default value for DOCKER_REPOSITORY in script

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-wallarm}
+
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag pgbouncer-config-reload $DOCKER_REPOSITORY/pgbouncer-config-reload:$TRAVIS_TAG
 docker push $DOCKER_REPOSITORY/pgbouncer-config-reload:$TRAVIS_TAG
